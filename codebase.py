53b23d60292188b81a069ef557f9cd69c627030b
Ann Anidumaka <annalexx2001@gmail.com>
2024-03-06 14:29:14 +0000

    wrote the full code after 100 days of python Day1

---
+++ codebase.py
@@ -0,0 +1,4 @@
+print('Welcome to Anns Band name generator \n')
+City = input('What city did you grow up in ? \n')
+Pet = input('What is the name of your pet\n')
+print('Suggested band name:' + ' '+ City + Pet + '!')
